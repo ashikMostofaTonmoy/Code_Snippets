67f65f68a9c6c55b0d7b6d1a595df36d212e54d5
ashikMostofaTonmoy <tonmoy.ashik@gmail.com>
2022-06-08 10:42:59 +0600

    added install instructions

---
+++ extractWeightFromFile.py
@@ -0,0 +1,21 @@
+
+# In[]:
+
+
+import pandas as pd
+import sys
+import platform
+
+print(platform.python_version())
+print(sys.path)
+
+# get_ipython().system('which python')
+# In[]:
+
+
+# df1= pd.read_csv('for model\kpt_rear.csv', index_col=0 )
+# df2= pd.read_csv('for model\kpt_side.csv', index_col=0)
+# df3= pd.read_csv('for model\segVal.csv', index_col=0)
+
+
+# In[]:
